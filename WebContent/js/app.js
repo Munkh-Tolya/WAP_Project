938e9c1bbb9fd1bf50be3844be0cfd8e1afa64f3
Namuun <s.namuun88@gmail.com>
2020-04-15 04:51:52 -0500

    Ajax remove from Shopping Cart - added

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -10,4 +10,40 @@ $(function(){
 			interval: 5000
 		});
 	}
+	
+	if($('#cart').hasClass('active')){
+		$(".remove").on("click",function(evt){
+			evt.preventDefault();
+			const dataItem = $(this).data("item");
+			const itemRow = $('#item-'+dataItem);
+			console.log($(itemRow).find('.total').html());
+			const itemPrice = parseFloat($(itemRow).find('.total').html().replace("$",""));
+			let gTotal = parseFloat($('#grandTotal').html());
+			showLoader();
+            fetch("cart/remove", 
+            {  method: 'POST',  
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: 'item='+dataItem
+            })
+            .then(res=>{
+                if(res.ok){
+	                itemRow.remove();
+	                gTotal -= itemPrice;
+	                $('#grandTotal').html(gTotal);
+                }else{
+                	alert("Please try again!");
+                }
+            })
+            .catch(error => showFailureInfo())
+            .then(hideLoader); 
+		});
+	}
 });
+function showLoader(){
+    $("#loader").show();
+}
+function hideLoader(){
+    $("#loader").hide();
+}
